59870c81e6b4ecc251569071321305e19357dba6
arnav-verma22 <arn.verma22@gmail.com>
2020-11-17 08:27:43 +0530

    importing dataset and removing unwanted columns

--- AImini.py
+++ AImini.py
@@ -8,41 +8,4 @@ print(df.head(5))
 y = df['Exited']
 x = df.drop(columns=['Exited', 'RowNumber', 'CustomerId', 'Surname'])
 
-from sklearn.preprocessing import LabelEncoder
-le = LabelEncoder()
-x['Gender'] = le.fit_transform(x['Gender'])
-print(x)
 
-'''from sklearn.preprocessing import OneHotEncoder
-from sklearn.compose import ColumnTransformer
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
-x = ct.fit_transform(x)'''
-
-dummies = pd.get_dummies(x['Geography'])
-x = pd.concat([x, dummies], axis=1)
-x.drop(columns=['Geography'], inplace=True)
-print(x)
-
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-x = sc.fit_transform(x)
-
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=0)
-
-ann = tf.keras.models.Sequential()
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-ann.add(tf.keras.layers.Dense(units=6, activation='relu'))
-ann.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
-
-ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-ann.fit(x_train, y_train, batch_size=32, epochs=100)
-
-y_pred = ann.predict(x_test)
-y_pred = (y_pred > 0.5)
-#print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
-from sklearn.metrics import confusion_matrix, accuracy_score
-cm = confusion_matrix(y_test, y_pred)
-print(cm)
-print(accuracy_score(y_test, y_pred))
